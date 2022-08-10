900dd4abf36c15241de116d0ed3b514f0952361d
Ricky Mario Landro <62229863+Ricky-mario-landro@users.noreply.github.com>
2022-08-10 12:19:42 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let btnOne = document.querySelector('#one');
+let btnTwo = document.querySelector('#two');
+let btnThree = document.querySelector('#three');
+
+btnOne.addEventListener('click', () => btnOne.style.backgroundColor='#FFFF00')
+btnTwo.addEventListener('click', () => btnTwo.style.backgroundColor='#FFFF00')
+btnThree.addEventListener('click', () => btnThree.style.backgroundColor='#FFFF00')
